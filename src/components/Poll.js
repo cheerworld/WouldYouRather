6bec93f94525a0712312e3b999e57200e6b85071
Yuguo Zhao <cheerzhao77@gmail.com>
2021-02-25 22:40:57 -0600

    refactor: add UnansweredPoll & answeredPoll component

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -1,24 +1,17 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import AnsweredPoll from "./AnsweredPoll";
+import UnansweredPoll from "./UnansweredPoll";
 
 class Poll extends Component {
   render() {
     console.log(this.props)
-    const {poll, user, userVote,dispatch } = this.props;
+    const {user, userVote, id} = this.props;
     return (
       <div>
-       {this.props.userVote!==null
-        ? (
-          <div>
-            <h3>Asked by {poll.name}</h3>
-            <p>{poll.avatar}</p>
-            <p>Would you rather {poll.option1}?</p>
-            <p>{poll.option1Votes} out of {poll.totalVotes} votes</p>
-            <p>Would you rather {poll.option2}?</p>
-            <p>{poll.option2Votes} out of {poll.totalVotes} votes</p>
-          </div>
-        )
-        :<p>No result yet</p>}
+       {this.props.userVote
+        ? <AnsweredPoll id={id}/>
+        : <UnansweredPoll id={id}/>}
       </div>
     )
   }
@@ -26,30 +19,13 @@ class Poll extends Component {
 
 
 function mapStateToProps({users, questions, authedUser}, {id}) {
-  const options = ["optionOne", "optionTwo"];
-  const name = users[questions[id].author].name;
-  const avatar = users[questions[id].author].avatarURL;
-  const option1 = questions[id].optionOne.text;
-  const option2 = questions[id].optionTwo.text;
-  const option1Votes = questions[id].optionOne.votes.length;
-  const option2Votes = questions[id].optionTwo.votes.length;
-  const totalVotes = option1Votes + option2Votes;
+
   const user = Object.keys(users[authedUser].answers);
-  const userVote = user.includes(id)
-    ? users[authedUser].answers[id]
-    : null;
+  const userVote = user.includes(id);
 
   return {
+    id,
     user,
-    poll: {
-      name,
-      avatar,
-      option1,
-      option2,
-      option1Votes,
-      option2Votes,
-      totalVotes,
-    },
     userVote,
   };
 }
---
+++ src/components/UnansweredPoll.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class UnansweredPoll extends Component {
+  state = {
+    option: "select",
+  };
+
+  handleChange = (e) => {
+    const option = e.target.value;
+    this.setState(() => ({
+      option,
+    }));
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const option = this.state.option;
+    console.log(option)
+    //this.props.dispatch()
+  }
+
+  render() {
+    console.log(this.props);
+    const { name, avatar, option1, option2 } = this.props.poll;
+    return (
+      <div>
+        <h3>name</h3>
+        <select value={this.state.option} onChange={this.handleChange}>
+          <option disabled value="select" hidden>
+            Select your option
+          </option>
+          <option value="optionOne">{option1}</option>
+          <option value="optionTwo">{option2}</option>
+        </select>
+        <button type="submit"
+          disabled={this.state.option === "select"}
+          onClick={this.handleSubmit}>
+          Sign In
+        </button>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ users, questions, authedUser }, { id }) {
+  const name = users[questions[id].author].name;
+  const avatar = users[questions[id].author].avatarURL;
+  const option1 = questions[id].optionOne.text;
+  const option2 = questions[id].optionTwo.text;
+  return {
+    poll: {
+      name,
+      avatar,
+      option1,
+      option2,
+    },
+  };
+}
+
+export default connect(mapStateToProps)(UnansweredPoll);
